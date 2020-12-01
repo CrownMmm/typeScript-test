7bb5e7a06c4c79b0395d50fd22351e4d0746e000
zhangjiaming <823600537@qq.com>
2020-12-02 02:07:17 +0800

    优化代码

--- demo.js
+++
@@ -1,6 +0,0 @@
-function tsDemo(data) {
-    console.log("hello");
-    // return Math.sqrt(data.x ** 2 + data.y ** 2);
-    return Math.sqrt(Math.pow(data.x, 2) + Math.pow(data.y, 2));
-}
-tsDemo({ x: 1, y: 2 });
